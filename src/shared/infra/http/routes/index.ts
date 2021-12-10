3a04b0cf388cccbb13fe15217f6e38ac93e2dfe8
Danilo Souza <danilo_souza19@outlook.com>
2021-12-10 15:54:39 -0300

    remove console.log

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,6 @@
-import { User } from '@modules/accounts/infra/entities/User';
 import { Router } from 'express';
 
 const routes = Router();
-
-console.log(User);
 
 routes.get('/', (request, response) => {
   return response.json({ ok: true });
