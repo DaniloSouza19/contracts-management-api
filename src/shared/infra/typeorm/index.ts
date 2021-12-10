3bf8d3ff3d5a95eef2b88c8f2350173498b29109
Danilo Souza <danilo_souza19@outlook.com>
2021-12-10 17:16:56 -0300

    add port

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { createConnection, Connection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database'): Promise<Connection> => {
+export default async (host = 'database', port = 5432): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
       host,
+      port,
     })
   );
 };
