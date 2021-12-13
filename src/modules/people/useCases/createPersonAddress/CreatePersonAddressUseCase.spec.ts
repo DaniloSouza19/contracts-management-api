cd7e264f10390ed06902f1026782b4048bd3a3d1
Danilo Souza <danilo_souza19@outlook.com>
2021-12-13 18:15:49 -0300

    add CreatePersonAddressUseCase test

---
+++ src/modules/people/useCases/createPersonAddress/CreatePersonAddressUseCase.spec.ts
@@ -0,0 +1,27 @@
+import { PeopleAddressRepositoryInMemory } from '@modules/people/repositories/in-memory/PeopleAddressRepositoryInMemory';
+
+import { CreatePersonAddressUseCase } from './CreatePersonAddressUseCase';
+
+let createPersonAddressUseCase: CreatePersonAddressUseCase;
+let peopleAddressRepositoryInMemory: PeopleAddressRepositoryInMemory;
+
+describe('Create a Person address', () => {
+  beforeEach(() => {
+    peopleAddressRepositoryInMemory = new PeopleAddressRepositoryInMemory();
+    createPersonAddressUseCase = new CreatePersonAddressUseCase(
+      peopleAddressRepositoryInMemory
+    );
+  });
+
+  it('Should be able to create a Person address', async () => {
+    const personAddress = await createPersonAddressUseCase.execute({
+      cep: '75000000',
+      city: 'Some City',
+      neighborhood: 'Some neighborhood',
+      state: 'GO',
+      street: 'Some street',
+    });
+
+    expect(personAddress).toHaveProperty('id');
+  });
+});
