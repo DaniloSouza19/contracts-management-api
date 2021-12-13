747474bf29c102b7262a0c92036ed565335f5f37
Danilo Souza <danilo_souza19@outlook.com>
2021-12-13 11:22:13 -0300

    add celebrate validation

--- src/modules/accounts/infra/routes/user.routes.ts
+++ src/modules/accounts/infra/routes/user.routes.ts
@@ -1,10 +1,21 @@
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { celebrate, Segments, Joi } from 'celebrate';
 import { Router } from 'express';
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
 
-usersRouter.post('/', createUserController.handle);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUserController.handle
+);
 
 export { usersRouter };
