79da149b7660b1efb57f9bc4aa549cd99d1b35eb
Danilo Souza <danilo_souza19@outlook.com>
2021-12-13 16:00:06 -0300

    add middleware ensureAuthenticated

---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '@shared/errors/AppError';
+
+async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token is missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(token, '97b2410ef3e86fa5b4d86ce23e115c45');
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id as string);
+
+    if (!user) {
+      throw new AppError('User does not exists', 401);
+    }
+
+    request.user = {
+      id: user.id,
+    };
+  } catch (error) {
+    throw new AppError('Invalid token', 401);
+  }
+
+  next();
+}
+
+export { ensureAuthenticated };
