5d11aece49d2fbfa9afb84a40e66400d07fa8ca5
Danilo Souza <danilo_souza19@outlook.com>
2021-12-13 18:17:37 -0300

    add peopleAddressRouter

---
+++ src/modules/people/infra/http/routes/peopleAddress.routes.ts
@@ -0,0 +1,31 @@
+import { CreatePersonAddressController } from '@modules/people/useCases/createPersonAddress/CreatePersonAddressController';
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+
+const peopleAddressRouter = Router();
+
+const createPersonAddressController = new CreatePersonAddressController();
+
+peopleAddressRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object({
+      cep: Joi.string()
+        .length(8)
+        .pattern(/^\d+$/, {
+          name: 'Only just numbers',
+        })
+        .required(),
+      street: Joi.string().required(),
+      state: Joi.string().required().length(2),
+      city: Joi.string().required(),
+      neighborhood: Joi.string().required(),
+    }),
+  }),
+  ensureAuthenticated,
+  createPersonAddressController.handle
+);
+
+export { peopleAddressRouter };
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { authenticateRouter } from '@modules/accounts/infra/routes/authenticate.routes';
 import { usersRouter } from '@modules/accounts/infra/routes/user.routes';
+import { peopleAddressRouter } from '@modules/people/infra/http/routes/peopleAddress.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', authenticateRouter);
+routes.use('/people-address', peopleAddressRouter);
 
 export { routes };
