02eb4ec2539d9561e944b1b760d90d3b32b391ae
Danilo Souza <danilo_souza19@outlook.com>
2021-12-14 17:20:05 -0300

    add migration of CreatePeople

---
+++ src/shared/infra/typeorm/migrations/1639508273986-CreatePeople.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePeople1639508273986 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'people',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'is_legal_person',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'document_id',
+            type: 'varchar',
+          },
+          {
+            name: 'telephone',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'address_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKPeopleAddress',
+            columnNames: ['address_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'people_address',
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('people');
+  }
+}
