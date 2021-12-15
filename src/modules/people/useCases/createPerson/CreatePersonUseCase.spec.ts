d867cd9240a49c0cefae3002104a763b8837b2c4
Danilo Souza <danilo_souza19@outlook.com>
2021-12-15 11:36:00 -0300

    add test already exists document_id | address_ad

--- src/modules/people/useCases/createPerson/CreatePersonUseCase.spec.ts
+++ src/modules/people/useCases/createPerson/CreatePersonUseCase.spec.ts
@@ -1,19 +1,35 @@
+import { PeopleAddressRepositoryInMemory } from '@modules/people/repositories/in-memory/PeopleAddressRepositoryInMemory';
 import { PeopleRepositoryInMemory } from '@modules/people/repositories/in-memory/PeopleRepositoryInMemory';
+
+import { AppError } from '@shared/errors/AppError';
 
 import { CreatePersonUseCase } from './CreatePersonUseCase';
 
 let createPersonUseCase: CreatePersonUseCase;
 let peopleRepositoryInMemory: PeopleRepositoryInMemory;
+let peopleAddressRepositoryInMemory: PeopleAddressRepositoryInMemory;
 
 describe('Create a Person', () => {
   beforeEach(() => {
     peopleRepositoryInMemory = new PeopleRepositoryInMemory();
-    createPersonUseCase = new CreatePersonUseCase(peopleRepositoryInMemory);
+    peopleAddressRepositoryInMemory = new PeopleAddressRepositoryInMemory();
+    createPersonUseCase = new CreatePersonUseCase(
+      peopleRepositoryInMemory,
+      peopleAddressRepositoryInMemory
+    );
   });
 
   it('Should be able to create a Person', async () => {
+    const address = await peopleAddressRepositoryInMemory.create({
+      cep: '75000000',
+      city: 'Some City',
+      neighborhood: 'Some neighborhood',
+      state: 'GO',
+      street: 'Some street',
+    });
+
     const person = await createPersonUseCase.execute({
-      address_id: 'some-address-id',
+      address_id: address.id,
       document_id: '000.000.000-00',
       name: 'John Doe',
       telephone: '(62) 9 9999-9999',
@@ -21,5 +37,45 @@ describe('Create a Person', () => {
     });
 
     expect(person).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a Person with already exists document_id', async () => {
+    const address = await peopleAddressRepositoryInMemory.create({
+      cep: '75000000',
+      city: 'Some City',
+      neighborhood: 'Some neighborhood',
+      state: 'GO',
+      street: 'Some street',
+    });
+
+    await expect(async () => {
+      await createPersonUseCase.execute({
+        address_id: address.id,
+        document_id: '000.000.000-00',
+        name: 'John Doe',
+        telephone: '(62) 9 9999-9999',
+        email: 'johndoe@example.com',
+      });
+
+      await createPersonUseCase.execute({
+        address_id: address.id,
+        document_id: '000.000.000-00',
+        name: 'John Doe',
+        telephone: '(62) 9 9999-9999',
+        email: 'johndoe@example.com',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to create a Person with non-existing address', async () => {
+    await expect(async () => {
+      await createPersonUseCase.execute({
+        address_id: 'invalid-address-id',
+        document_id: '000.000.000-00',
+        name: 'John Doe',
+        telephone: '(62) 9 9999-9999',
+        email: 'johndoe@example.com',
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
