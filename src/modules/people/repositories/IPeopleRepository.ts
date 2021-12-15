82829a35b6dbafca43518d630bd8f1f5f2009d5f
Danilo Souza <danilo_souza19@outlook.com>
2021-12-15 11:32:32 -0300

    add findByDocumentId method

--- src/modules/people/repositories/IPeopleRepository.ts
+++ src/modules/people/repositories/IPeopleRepository.ts
@@ -3,6 +3,7 @@ import { Person } from '../infra/typeorm/entities/Person';
 
 interface IPeopleRepository {
   create(data: ICreatePersonDTO): Promise<Person>;
+  findByDocumentId(document_id: string): Promise<Person | undefined>;
 }
 
 export { IPeopleRepository };
