38427321e9e3047f4ad85875eb0401eddc9bdb38
Danilo Souza <danilo_souza19@outlook.com>
2021-12-15 11:33:50 -0300

    add findById method

--- src/modules/people/repositories/IPeopleAddressRepository.ts
+++ src/modules/people/repositories/IPeopleAddressRepository.ts
@@ -3,6 +3,7 @@ import { PersonAddress } from '../infra/typeorm/entities/PersonAddress';
 
 interface IPeopleAddressRepository {
   create(data: ICreatePersonAddressDTO): Promise<PersonAddress>;
+  findById(id: string): Promise<PersonAddress | undefined>;
 }
 
 export { IPeopleAddressRepository };
