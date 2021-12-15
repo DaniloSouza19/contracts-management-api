a23c8c16a0078ccefe60c1590d54d687f975ce39
Danilo Souza <danilo_souza19@outlook.com>
2021-12-15 11:36:49 -0300

    register PeopleRepository

--- src/modules/people/container/index.ts
+++ src/modules/people/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe';
 
 import { PeopleAddressRepository } from '../infra/typeorm/repositories/PeopleAddressRepository';
+import { PeopleRepository } from '../infra/typeorm/repositories/PeopleRepository';
 import { IPeopleAddressRepository } from '../repositories/IPeopleAddressRepository';
+import { IPeopleRepository } from '../repositories/IPeopleRepository';
 
 container.registerSingleton<IPeopleAddressRepository>(
   'PeopleAddressRepository',
   PeopleAddressRepository
+);
+
+container.registerSingleton<IPeopleRepository>(
+  'PeopleRepository',
+  PeopleRepository
 );
