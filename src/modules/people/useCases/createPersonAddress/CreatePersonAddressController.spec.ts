c1b29cd7451e87cb14bfd5853fff79ff1825bb2e
Danilo Souza <danilo_souza19@outlook.com>
2021-12-22 18:03:07 -0300

    test: CreatePersonAddressController

---
+++ src/modules/people/useCases/createPersonAddress/CreatePersonAddressController.spec.ts
@@ -0,0 +1,62 @@
+import { hash } from 'bcrypt';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+const API_PREFIX = '/api/v1';
+let connection: Connection;
+let token: string;
+
+describe('Create a User', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `insert into users(id, name, email, password, is_admin, created_at, updated_at)
+        values ('${id}', 'admin', 'admin@dgs.com.br', '${password}', true, now(), now());
+      `
+    );
+
+    const responseSession = await request(app)
+      .post(`${API_PREFIX}/sessions`)
+      .send({
+        email: 'admin@dgs.com.br',
+        password: 'admin',
+      });
+
+    token = responseSession.body.token;
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('Should be able to create an user', async () => {
+    const response = await request(app)
+      .post(`${API_PREFIX}/people-address`)
+      .send({
+        postal_code: '75000000',
+        street: 'Some street',
+        state: 'GO',
+        city: 'Some City',
+        neighborhood: 'Some neighborhood',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.postal_code).toBe('75000000');
+    expect(response.body.street).toBe('Some street');
+  });
+});
