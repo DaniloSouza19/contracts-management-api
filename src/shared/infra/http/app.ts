db899edf0e3869a34ea882ba9748d6c0c52202b4
Danilo Souza <danilo_souza19@outlook.com>
2021-12-22 16:24:43 -0300

    make api version onto patch

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -18,12 +18,12 @@ createConnection();
 
 app.use(express.json());
 
-app.use(routes);
+app.use('/api/v1', routes);
 
 /**
  * Swagger-Ui middleware documentation route
  */
-app.use('/api-docs/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/docs/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /**
  * Celebrate middleware
