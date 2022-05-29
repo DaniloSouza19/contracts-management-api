2171eb1c237395bef9a9de1c61e5ebb3236f2bc5
Danilo Souza <danilo_souza19@outlook.com>
2022-05-29 20:01:36 -0300

    chore: add .env variable

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -10,12 +10,14 @@ async function create(): Promise<void> {
   const connection = await createConnection(HOST, PORT);
 
   const id = uuidV4();
-  const password = await hash('admin', 8);
+  const password = await hash(process.env.ADMIN_USER_PASSWORD || 'admin', 8);
+  const name = process.env.ADMIN_USER_NAME || 'admin';
+  const email = process.env.ADMIN_USER_EMAIL || 'admin@dgs.com.br';
 
   try {
     await connection.query(
       `INSERT INTO users(id, name, email, password, created_at, updated_at, is_admin)
-        VALUES ('${id}','admin', 'admin@dgs.com.br','${password}', now(), now(), true)`
+        VALUES ('${id}','${name}', '${email}','${password}', now(), now(), true)`
     );
   } catch (error) {
     console.log(error);
